d925bf2961af3dedeeb54c45bbec7ca1508f0637
Adi Venkat <68367441+adivenkat05@users.noreply.github.com>
2022-12-29 23:16:25 +0530

    Functions

---
+++ function_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int func(int a, int b) // Function Prototype
+
+int main(){
+
+func(5, 6); // Function Calling.
+
+} 
+
+int func(int a, int b){ 
+    printf("%d %d", a, b); // Function Declaration
+    
+}
